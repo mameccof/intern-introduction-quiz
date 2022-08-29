fe6a7a89037732c56f81ae0892d8ad46e6e40edd
ArataYoshida <>
2022-08-29 18:52:48 +0900

    ログイン情報の登録

--- app/src/app/pages/sign-up/sign-up.component.ts
+++ app/src/app/pages/sign-up/sign-up.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from '../../services/user.service';
+
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+
 
 @Component({
   selector: 'app-sign-up',
@@ -7,11 +12,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignUpComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private userService: UserService,
+    private http: HttpClient,
+    ) { }
 
   ngOnInit(): void {
   }
 
   isHide = true;
+
+  user = {
+    mailadress: '',
+    password: '',
+    user_name: '',
+    birth_date: '',
+    birth_place: '',
+    affilition : '',
+  }
+
+  registerUser(): void{
+
+    this.userService.addUser(this.user).subscribe(() => {
+      console.log("bbb");
+      })
+  }
 
 }
