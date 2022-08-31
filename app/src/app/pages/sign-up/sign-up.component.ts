66fa6af3821247ba7d32b57a480ca6275d1ee606
ArataYoshida <>
2022-08-31 18:08:22 +0900

    余分なコードの削除

--- app/src/app/pages/sign-up/sign-up.component.ts
+++ app/src/app/pages/sign-up/sign-up.component.ts
@@ -1,10 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService } from '../../services/user.service';
 
-import { HttpClient, HttpHeaders } from '@angular/common/http';
-
-
-
 @Component({
   selector: 'app-sign-up',
   templateUrl: './sign-up.component.html',
@@ -14,7 +10,6 @@ export class SignUpComponent implements OnInit {
 
   constructor(
     private userService: UserService,
-    private http: HttpClient,
     ) { }
 
   ngOnInit(): void {
