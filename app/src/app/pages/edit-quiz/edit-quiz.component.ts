babb880acd8fe14f698f37d597a9fa05439b8562
ArataYoshida <>
2022-09-26 23:37:57 +0900

    レビューの修正　メソッド名・caa

--- app/src/app/pages/edit-quiz/edit-quiz.component.ts
+++ app/src/app/pages/edit-quiz/edit-quiz.component.ts
@@ -6,90 +6,88 @@ import { Question } from 'src/app/types/types';
 @Component({
   selector: 'app-edit-quiz',
   templateUrl: './edit-quiz.component.html',
-  styleUrls: ['./edit-quiz.component.scss']
+  styleUrls: ['./edit-quiz.component.scss'],
 })
 export class EditQuizComponent implements OnInit {
-
   quiz: Question = {
-    question: "",
-    explanation: "",
+    question: '',
+    explanation: '',
     selection: [
       {
-        sentence: "",
+        sentence: '',
         is_correct: true,
         sort_num: 1,
       },
       {
-        sentence: "",
+        sentence: '',
         is_correct: false,
         sort_num: 2,
       },
       {
-        sentence: "",
+        sentence: '',
         is_correct: false,
         sort_num: 3,
       },
       {
-        sentence: "",
+        sentence: '',
         is_correct: false,
         sort_num: 4,
-      }
+      },
     ],
-    order: parseInt(localStorage.getItem("question_num") ?? "0") + 1,
-    user: parseInt(localStorage.getItem("loginUser") ?? "0"),
-  }
+    order: parseInt(localStorage.getItem('question_num') ?? '0') + 1,
+    user: parseInt(localStorage.getItem('loginUser') ?? '0'),
+  };
 
   constructor(
     private quizService: QuizService,
     private route: ActivatedRoute,
     private router: Router
-  ) { }
+  ) {}
 
   ngOnInit(): void {
     const id = Number(this.route.snapshot.paramMap.get('id'));
-    if(id){
-      this.quizService.getQuiz(id).subscribe( quiz => {
+    if (id) {
+      this.quizService.getQuiz(id).subscribe((quiz) => {
         this.quiz = quiz;
-      } );
-    }else{
-      this.quizService.getTemplate().subscribe( template => {
+      });
+    } else {
+      this.quizService.getTemplate().subscribe((template) => {
         this.quiz.question = template.question;
         this.quiz.selection = template.selection;
-        this.quiz.explanation = "解説文はありません";
+        this.quiz.explanation = '解説文はありません';
       });
     }
   }
 
-  registerButton(){
-    if(this.quiz.id){
-      this.quizService.putQuiz(this.quiz,this.quiz.id ?? 0).subscribe( q => {
+  registerQuiz() {
+    if (this.quiz.id) {
+      this.quizService.putQuiz(this.quiz, this.quiz.id ?? 0).subscribe((q) => {
         this.router.navigate(['/home']);
-      })
-    }else{
-      this.quizService.postQuiz(this.quiz).subscribe( q => {
+      });
+    } else {
+      this.quizService.postQuiz(this.quiz).subscribe((q) => {
         this.router.navigate(['/home']);
-      })
+      });
     }
   }
 
   //2回同じテンプレを引かないようにしたい
-  randomButton(){
-    this.quizService.getTemplate().subscribe(template => {
-      if(this.quiz.question === template.question){
-        this.randomButton();
-      }else{
+  randomQuiz() {
+    this.quizService.getTemplate().subscribe((template) => {
+      if (this.quiz.question === template.question) {
+        this.randomQuiz();
+      } else {
         this.quiz.question = template.question;
         this.quiz.selection = template.selection;
-        this.quiz.explanation = "解説文はありません";
+        this.quiz.explanation = '解説文はありません';
       }
     });
   }
 
-  setCorrect(i: number){
-    for(let selection of this.quiz.selection){
+  setCorrect(i: number) {
+    for (let selection of this.quiz.selection) {
       selection.is_correct = false;
     }
     this.quiz.selection[i].is_correct = true;
   }
-
 }
