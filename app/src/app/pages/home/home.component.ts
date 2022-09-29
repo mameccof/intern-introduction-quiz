334cc90a304570c6c0ed20c0e64554d1e64a7c9f
ArataYoshida <>
2022-09-29 17:55:00 +0900

    変数名の変更　ソートの処理の変更 コードの行の入れ替え

--- app/src/app/pages/home/home.component.ts
+++ app/src/app/pages/home/home.component.ts
@@ -5,71 +5,50 @@ import { QrDialogComponent } from '../../components/qr-dialog/qr-dialog.componen
 import { UserService } from '../../services/user.service';
 import { QuizService } from 'src/app/services/quiz.service';
 
-interface quiz{
-  question:string
-  explanation:string
-  selection: {
-    sentence:string
-    is_correct:boolean
-    sort_num:number
-  }[]
-}[]
-
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
-
-  questions: Question[] = []
-  loginUser!: User
-
-  //ソートの時、問題自体を仮置きする変数
-  tmpObj: Question = {
-    id: 0,
-    question: '',
-    explanation: '',
-    selection: [],
-    order: 0
-  }
+  sortedMyQuestions: Question[] = [];
+  loginUser!: User;
 
   constructor(
     public dialog: MatDialog,
     private userService: UserService,
-    private quizService: QuizService,
-  ) { }
+    private quizService: QuizService
+  ) {}
 
   ngOnInit(): void {
-    this.userService.getUser().subscribe(
-      user =>{
+    this.userService.getUser().subscribe((user) => {
+      localStorage.setItem('loginUser', user.id.toString());
 
-        localStorage.setItem('loginUser', user.id.toString());
+      this.loginUser = user;
+      if (this.loginUser.icon_url === undefined) {
+        this.loginUser.icon_url =
+          'https://material.angular.io/assets/img/examples/shiba2.jpg';
+      }
 
-        this.loginUser = user
-        if(this.loginUser.icon_url === undefined){
-          this.loginUser.icon_url = "https://material.angular.io/assets/img/examples/shiba2.jpg"
-        }
-
-        //データベースから取得したデータはorder順になっていないので、orderの昇順になるようにソート
-        for(let i = 0; i < this.loginUser.questions.length - 1; i++){
-          for(let j = this.loginUser.questions.length - 1;j>i; j--){
-            if(this.loginUser.questions[j-1].order > this.loginUser.questions[j].order){
-              this.tmpObj = this.loginUser.questions[j - 1];
-              this.loginUser.questions[j - 1] = this.loginUser.questions[j];
-              this.loginUser.questions[j] = this.tmpObj;
-            }
-          }
-        }
-        localStorage.setItem('question_num', this.questions.length.toString());
-
-        //questionsにデータを入れてリスト表示しているので、ソート済みのloginUser.questionsを入れることによってorderの昇順表示ができる
-        this.questions = this.loginUser.questions;
-
-        //問題を削除した時にイベントを渡すと、リロードの関係でうまくいってるのかよくわからないので
-        //問題削除 > リロード > homeの読み込み >  ngOnInit() > updateOrder() 問題が削除された時にorderがずれることを対策
-        this.updateOrder();
+      //データベースから取得したデータはorder順になっていないので、orderの昇順になるようにソート
+      const sortedQuizzes = this.loginUser.questions.sort(function (
+        first,
+        second
+      ) {
+        return first.order - second.order;
       });
+
+      this.sortedMyQuestions = sortedQuizzes;
+
+      localStorage.setItem(
+        'question_num',
+        this.sortedMyQuestions.length.toString()
+      );
+
+      //問題を削除した時にイベントを渡すと、リロードの関係でうまくいってるのかよくわからないので
+      //問題削除 > リロード > homeの読み込み >  ngOnInit() > updateOrder() 問題が削除された時にorderがずれることを対策
+      this.updateOrder();
+    });
   }
 
   openDialog(): void {
@@ -84,10 +63,15 @@ export class HomeComponent implements OnInit {
 
   //ドロップリストをいじると、その中に入っていた配列の添字も一緒に変更されていた。
   //上から順番にorderを割り振ればリストの問題とorderを同期させることができる
-  updateOrder(){
-    for(let i = 1;i<=this.questions.length;i++){
-      this.questions[i-1].order = i;
-      this.quizService.putQuiz(this.questions[i-1],this.questions[i-1].id).subscribe();
+  updateOrder() {
+    for (let i = 1; i <= this.sortedMyQuestions.length; i++) {
+      this.sortedMyQuestions[i - 1].order = i;
+      this.quizService
+        .putQuiz(
+          this.sortedMyQuestions[i - 1],
+          this.sortedMyQuestions[i - 1].id
+        )
+        .subscribe();
     }
   }
 }
