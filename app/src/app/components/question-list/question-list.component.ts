44024de5ca3975f2f7f80c5c67f47bfa301a78fc
ArataYoshida <>
2022-10-13 17:21:39 +0900

    確認ダイヤログの追加

--- app/src/app/components/question-list/question-list.component.ts
+++ app/src/app/components/question-list/question-list.component.ts
@@ -1,9 +1,9 @@
-import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
-import {CdkDragDrop, moveItemInArray} from '@angular/cdk/drag-drop';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { CdkDragDrop, moveItemInArray } from '@angular/cdk/drag-drop';
 import { QuizService } from '../../services/quiz.service';
 import { Question } from 'src/app/types/types';
-import { outputAst } from '@angular/compiler';
-
+import { MatDialog } from '@angular/material/dialog';
+import { ConfirmDialogComponent } from '../confirm-dialog/confirm-dialog.component';
 
 /**
  * @title Drag&Drop sorting
@@ -11,20 +11,16 @@ import { outputAst } from '@angular/compiler';
 @Component({
   selector: 'app-question-list',
   templateUrl: './question-list.component.html',
-  styleUrls: ['./question-list.component.scss']
+  styleUrls: ['./question-list.component.scss'],
 })
 export class QuestionListComponent implements OnInit {
-
-  @Input() questions?:Question[];
+  @Input() questions?: Question[];
 
   @Output() updateOrderEvent = new EventEmitter();
 
-  constructor(
-    private quizService: QuizService,
-  ) { }
+  constructor(private quizService: QuizService, public dialog: MatDialog) {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   drop(event: CdkDragDrop<string[]>) {
     moveItemInArray(this.questions!, event.previousIndex, event.currentIndex);
@@ -32,10 +28,21 @@ export class QuestionListComponent implements OnInit {
     this.updateOrderEvent.emit();
   }
 
-  deleteQuestion(questionId:number){
-    this.quizService.deleteQuestion(questionId).subscribe( () => {
+  deleteQuestion(questionId: number) {
+    this.quizService.deleteQuestion(questionId).subscribe(() => {
       window.location.reload();
     });
   }
 
+  openDialog(Id: number) {
+    const dialogRef = this.dialog.open(ConfirmDialogComponent, {
+      width: '300px',
+    });
+
+    dialogRef.afterClosed().subscribe((isDelete) => {
+      if (isDelete) {
+        this.deleteQuestion(Id);
+      }
+    });
+  }
 }
