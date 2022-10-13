c1ac7217bd0ea3b39d6193a38f2f6f7810e28cfb
ArataYoshida <>
2022-10-13 18:45:36 +0900

    QRの表示・ページ遷移

--- app/src/app/components/qr-dialog/qr-dialog.component.ts
+++ app/src/app/components/qr-dialog/qr-dialog.component.ts
@@ -1,19 +1,19 @@
 import { Component, OnInit } from '@angular/core';
-import { MatDialogRef} from '@angular/material/dialog';
-
+import { MatDialogRef } from '@angular/material/dialog';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-qr-dialog',
   templateUrl: './qr-dialog.component.html',
 })
 export class QrDialogComponent implements OnInit {
+  startURL: string = `${environment.FRONT_URL}/start/${localStorage.getItem(
+    'loginUserId'
+  )}`;
 
-  constructor(
-    public dialogRef: MatDialogRef<QrDialogComponent>,
-  ) {}
+  constructor(public dialogRef: MatDialogRef<QrDialogComponent>) {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   onNoClick(): void {
     this.dialogRef.close();
--- app/src/environments/environment.ts
+++ app/src/environments/environment.ts
@@ -5,6 +5,7 @@
 export const environment = {
   production: false,
   API_URL: 'http://localhost:1337',
+  FRONT_URL: 'http://localhost:4200',
 };
 
 /*
